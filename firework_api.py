54e5acd676a647480ca4e53f3cfc89ca0f027859
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2024-10-14 18:39:02 +0330

    added new functions

--- firework_api.py
+++ firework_api.py
@@ -1,18 +1,55 @@
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
 
-client = OpenAI(
- base_url="http://localhost:11434/v1/",
-)
+CHAT_MODEL_NAME = "accounts/fireworks/models/llama-v3p1-8b-instruct"
+EMBED_MODEL_NAME = "nomic-ai/nomic-embed-text-v1.5"
 
-response = client.chat.completions.create(
- messages=[
-     {
-         "role": "user",
-         "content": "Say this is a test",
-     }
- ],
- # notice the change in the model name
- model="llama3.2",
-)
+def get_openai_client():
+    load_dotenv()
+    try:
+        openai_client = OpenAI(
+        api_key=os.environ.get("FIREWORKS_API_KEY"),
+        base_url=os.environ.get("FIREWORKS_API_BASE"),
+        )
 
-print(response.choices[0].message.content)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    return openai_client
+
+def get_openai_embedding(openai_client, text):
+    response = openai_client.embeddings.create(
+        model=EMBED_MODEL_NAME,
+        input=text
+        )
+    embedding = response.data[0].embedding
+    print("==== Generated an embedding... ====")
+    return embedding
+
+
+def generate_response(openai_client, question, relevant_chunks):
+    context = "\n\n".join(relevant_chunks)
+    prompt = (
+        "You are an assistant for question-answering tasks. Use the following pieces of "
+        "retrieved context to answer the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the answer concise."
+        "\n\nContext:\n" + context + "\n\nQuestion:\n" + question
+    )
+
+    response = openai_client.chat.completions.create(
+        model=CHAT_MODEL_NAME,
+        messages=[
+            {
+                "role": "system",
+                "content": prompt,
+            },
+            {
+                "role": "user",
+                "content": question,
+            },
+        ],
+    )
+
+    answer = response.choices[0].message.content
+    return answer
